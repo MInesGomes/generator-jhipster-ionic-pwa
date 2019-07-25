1f3aedc6f8d332dd6483e87fb4194b56654deaa0
M. Ines Gomes <siteGoTop@gmail.com>
2019-07-25 07:10:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,180 @@
+# Ionic for JHipster
+[![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> A JHipster Module that generates an Ionic Client
+
+## Introduction
+
+This is a [JHipster](http://www.jhipster.tech/) module. You can use it to generate an Ionic app that talks to a JHipster backend. It uses the [Ionic JHipster Starter](https://github.com/oktadeveloper/ionic-jhipster-starter) as a base template, then applies functionality (e.g. entity generation) on top of it. 
+
+To learn more about this module, see [Use Ionic for JHipster to Create Mobile Apps with OIDC Authentication](https://developer.okta.com/blog/2018/01/30/jhipster-ionic-with-oidc-authentication).
+
+## Prerequisites
+
+As this is a [JHipster](http://www.jhipster.tech/) module, we expect you to have an existing JHipster app and Ionic already installed.
+
+- [Installing JHipster](https://www.jhipster.tech/installation.html)
+- [Installing Ionic](https://ionicframework.com/docs/intro/installation/)
+
+Or simply:
+
+```bash
+npm i -g generator-jhipster ionic
+```
+
+## Installation
+
+To install this module:
+
+```bash
+npm install -g generator-jhipster-ionic yo
+```
+
+To update this module:
+
+```bash
+npm update -g generator-jhipster-ionic
+```
+
+## Usage
+
+Create a JHipster app using `yo jhipster`, or a backend-only with `yo jhipster:server`. Choose `JWT` or `OAuth 2.0` as the authentication type. 
+
+In a directory alongside your JHipster app, run this module. This will create an Ionic app for you and install the necessary JHipster files for it to communicate with the backend.
+
+```bash
+yo jhipster-ionic
+```
+
+### Okta for Authentication
+
+Choosing OAuth 2.0 / OIDC for authentication will allow you to use Keycloak or Okta for identity. In theory, you should be able to use any OIDC-compliant identity provider, and these are the only ones we've tested against. JHipster ships with Keycloak configured and ready to go by default. You simply have to start it in your JHipster backend.
+
+```bash
+docker-compose -f src/main/docker/keycloak up
+```
+
+See [JHipster's security docs](https://www.jhipster.tech/security/#-oauth2-and-openid-connect) to see how to configure JHipster for Okta. 
+
+In addition to having a OIDC app for your JHipster backend, you'll need to create a Native app too.
+
+#### Create a Native Application in Okta
+
+Log in to your Okta Developer account (or [sign up](https://developer.okta.com/signup/) if you don't have an account).
+
+From the **Applications** page, choose **Add Application**. On the Create New Application page, select **Native**. Give your app a memorable name, and configure it as follows:
+ 
+* Login redirect URIs: 
+  * `http://localhost:8100/implicit/callback`
+  * `dev.localhost.ionic:/callback`
+* Logout redirect URIs:
+  * `http://localhost:8100/implicit/logout`
+  * `dev.localhost.ionic:/logout`
+  
+**NOTE:** `dev.localhost.ionic` is the default scheme, but you can also use something more traditional like `com.okta.dev-737523` (where `dev-737523.okta.com` is your Okta Org URL). If you change it, be sure to update the `URL_SCHEME` in `package.json` and the redirect URIs in `src/app/auth/auth.service.ts`.
+
+```json
+"cordova-plugin-customurlscheme": {
+    "URL_SCHEME": "com.okta.dev-737523"
+},
+```
+
+Open `src/app/auth/auth.service.ts` in an editor, search for `data.clientId` and replace it with the client ID from your Native app.
+
+```ts
+// try to get the oauth settings from the server
+this.requestor.xhr({method: 'GET', url: AUTH_CONFIG_URI}).then(async (data: any) => {
+  this.authConfig = {
+    identity_client: '{yourClientId}',
+    identity_server: data.issuer,
+    redirect_url: redirectUri,
+    end_session_redirect_url: logoutRedirectUri,
+    scopes,
+    usePkce: true
+  };
+  ...
+}
+```
+
+#### Add Claims to Access Token
+
+In order to authentication successfully with your Ionic app, you have to do a bit more configuration in Okta. Since the Ionic client will only send an access token to JHipster, you need to 1) add a `groups` claim to the access token and 2) add a couple more claims so the user's name will be available in JHipster.
+
+Navigate to **API** > **Authorization Servers**, click the **Authorization Servers** tab and edit the **default** one. Click the **Claims** tab and **Add Claim**. Name it "groups" and include it in the Access Token. Set the value type to "Groups" and set the filter to be a Regex of `.*`. Click **Create**.
+
+Add another claim, name it `given_name`, include it in the access token, use `Expression` in the value type, and set the value to `user.firstName`. Optionally, include it in the `profile` scope. Perform the same actions to create a `family_name` claim and use expression `user.lastName`.
+
+### iOS 
+
+Generate a native project with the following command:
+
+```
+ionic cordova prepare ios
+```
+
+Open your project in Xcode, configure code signing, and run your app.
+
+```
+open platforms/ios/MyApp.xcworkspace
+```
+
+### Android
+
+Generate a native project with the following command:
+
+```
+ionic cordova prepare android
+```
+
+Set the launchMode to `singleTask` so the URL does not trigger a new instance of the app in `platforms/android/app/src/main/AndroidManifest.xml`:
+
+```xml
+<activity
+      android:configChanges="orientation|keyboardHidden|keyboard|screenSize|locale"
+      android:name="com.mydomain.app.MainActivity"
+      android:label="@string/title_activity_main"
+      android:launchMode="singleTask"
+      android:theme="@style/AppTheme.NoActionBarLaunch">
+```
+
+Open your project in Android Studio and run your app.
+
+```
+studio platforms/android
+```
+
+You'll need to run a couple commands to allow the emulator to communicate with your API and Keycloak.
+
+```
+adb reverse tcp:8080 tcp:8080
+adb reverse tcp:9080 tcp:9080
+```
+
+### Entity Generator
+
+To generate entities, run `yo jhipster-ionic:entity <name>`.
+
+## Testing
+
+You can run unit tests with:
+
+```
+npm test
+```
+
+See the [testing section](https://github.com/oktadeveloper/ionic-jhipster-starter#testing) of the Ionic JHipster Starter for more information.
+
+## Contributing
+
+Please read our [guidelines](/CONTRIBUTING.md#submitting-an-issue) before submitting an issue. If your issue is a bug, please use the bug template pre populated [here](https://github.com/jhipster/generator-jhipster-ionic/issues/new). For feature requests and queries you can use [this template][feature-template].
+
+## License
+
+Apache-2.0 © [Okta, Inc](https://developer.okta.com)
+
+[npm-image]: https://img.shields.io/npm/v/generator-jhipster-ionic.svg
+[npm-url]: https://npmjs.org/package/generator-jhipster-ionic
+[travis-image]: https://travis-ci.org/oktadeveloper/generator-jhipster-ionic.svg?branch=master
+[travis-url]: https://travis-ci.org/oktadeveloper/generator-jhipster-ionic
+[daviddm-image]: https://david-dm.org/oktadeveloper/generator-jhipster-ionic.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/oktadeveloper/generator-jhipster-ionic
+[feature-template]: https://github.com/oktadeveloper/generator-jhipster-ionic/issues/new?body=*%20**Overview%20of%20the%20request**%0A%0A%3C!--%20what%20is%20the%20query%20or%20request%20--%3E%0A%0A*%20**Motivation%20for%20or%20Use%20Case**%20%0A%0A%3C!--%20explain%20why%20this%20is%20a%20required%20for%20you%20--%3E%0A%0A%0A*%20**Browsers%20and%20Operating%20System**%20%0A%0A%3C!--%20is%20this%20a%20problem%20with%20all%20browsers%20or%20only%20IE8%3F%20--%3E%0A%0A%0A*%20**Related%20issues**%20%0A%0A%3C!--%20has%20a%20similar%20issue%20been%20reported%20before%3F%20--%3E%0A%0A*%20**Suggest%20a%20Fix**%20%0A%0A%3C!--%20if%20you%20can%27t%20fix%20this%20yourself%2C%20perhaps%20you%20can%20point%20to%20what%20might%20be%0A%20%20causing%20the%20problem%20(line%20of%20code%20or%20commit)%20--%3E
---
+++ generators/app/index.js
@@ -0,0 +1,282 @@
+/**
+ * Copyright 2013-2019 the original author or authors from the JHipster project.
+ *
+ * This file is part of the JHipster project, see http://www.jhipster.tech/
+ * for more information.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/* eslint-disable consistent-return */
+const chalk = require('chalk');
+const packagejs = require('../../package.json');
+const jsonfile = require('jsonfile');
+const semver = require('semver');
+const shelljs = require('shelljs');
+const BaseGenerator = require('generator-jhipster/generators/generator-base');
+const spawn = require('cross-spawn');
+const fs = require('fs');
+const constants = require('generator-jhipster/generators/generator-constants');
+const utils = require('./utils');
+
+module.exports = class extends BaseGenerator {
+  constructor(args, opts) {
+    super(args, opts);
+
+    this.configOptions = {};
+    // This adds support for a `--interactive` flag
+    this.option('interactive', {
+      desc: 'Don\'t prompt user when running ionic start',
+      type: Boolean,
+      defaults: true
+    });
+
+    // This adds support for a `--install` flag
+    this.option('installDeps', {
+      desc: 'Don\'t install dependencies when running ionic start',
+      type: Boolean,
+      defaults: true
+    });
+
+    this.interactive = this.options.interactive;
+    this.installDeps = this.options.installDeps;
+  }
+
+  get initializing() {
+    return {
+      init(args) {
+        if (args === 'default') {
+          this.defaultApp = true;
+          this.interactive = false;
+        }
+      },
+      readConfig() {
+        this.jhipsterAppConfig = this.getAllJhipsterConfig();
+      },
+      displayLogo() {
+        // it's here to show that you can use functions from generator-jhipster
+        // this function is in: generator-jhipster/generators/generator-base.js
+        // this.printJHipsterLogo();
+
+        // Have Yeoman greet the user.
+        this.log(`\nWelcome to the ${chalk.bold.blue('Ionic')} Module for ${chalk.bold.green('J')}${chalk.bold.red('Hipster')}! ${chalk.yellow(`v${packagejs.version}\n`)}`);
+      }
+    };
+  }
+
+  prompting() {
+    const done = this.async();
+    const messageAskForPath = 'Enter the directory where your JHipster app is located:';
+    const prompts = [
+      {
+        type: 'input',
+        name: 'appName',
+        message: 'What do you want to name your Ionic application?',
+        default: 'ionic4j'
+      },
+      {
+        type: 'input',
+        name: 'directoryPath',
+        message: messageAskForPath,
+        default: 'backend',
+        validate: (input) => {
+          const path = this.destinationPath(input);
+          if (shelljs.test('-d', path)) {
+            const appsFolders = utils.getAppFolder.call(this, input);
+            if (appsFolders.length === 0) {
+              return `No application found in ${path}`;
+            }
+            return true;
+          }
+          return `${path} is not a directory or doesn't exist`;
+        }
+      }];
+    if (this.defaultApp) {
+      this.ionicAppName = 'ionic4j';
+      this.directoryPath = 'backend';
+      done();
+    } else {
+      this.prompt(prompts).then((props) => {
+        this.ionicAppName = props.appName;
+        this.directoryPath = props.directoryPath;
+        done();
+      });
+    }
+  }
+
+  writing() {
+    const fromPath = `${this.directoryPath}/.yo-rc.json`;
+    this.jhipsterAppConfig = this.fs.readJSON(fromPath)['generator-jhipster'];
+
+    const currentJhipsterVersion = this.jhipsterAppConfig.jhipsterVersion;
+    const minimumJhipsterVersion = packagejs.dependencies['generator-jhipster'];
+    if (currentJhipsterVersion !== undefined && !semver.satisfies(currentJhipsterVersion, minimumJhipsterVersion)) {
+      this.error(`\nYour backend uses an old JHipster version (${currentJhipsterVersion})... you need at least (${minimumJhipsterVersion})\n`);
+    }
+
+    this.isIonicV3 = this._isIonicV3();
+    if (this.isIonicV3) {
+      this.log(`\n${chalk.bold.red('You are not using the latest version of Ionic. Run npm install -g ionic')}`);
+    }
+
+    const applicationType = this.jhipsterAppConfig.applicationType;
+    if (applicationType !== 'monolith' && applicationType !== 'gateway') {
+      this.error(`\nYour backend project must be a monolith or a gateway to work with this module! Found application type: ${applicationType}.\n`);
+    }
+
+    const cmd = `ionic start ${this.ionicAppName} oktadeveloper/jhipster${(this.interactive) ? '' : ' --no-interactive'}`;
+    this.log(`\nCreating Ionic app with command: ${chalk.green(`${cmd}`)}`);
+    const params = ['start', this.ionicAppName, 'oktadeveloper/jhipster'];
+    if (!this.interactive) {
+      params.push('--no-interactive');
+      params.push('--quiet');
+    }
+    if (!this.installDeps) {
+      params.push('--no-deps');
+      params.push('--no-git');
+    }
+    spawn.sync('ionic', params, {stdio: 'inherit'});
+  }
+
+  _isIonicV3() {
+    const currentIonicVersion = shelljs.exec('ionic version --no-interactive', {silent: true}).stdout.replace(/\n/g, '');
+    const minimumIonicVersion = '<4.0.0';
+
+    return semver.satisfies(currentIonicVersion, minimumIonicVersion);
+  }
+
+  install() {
+    // update package.json in Ionic app
+    const done = this.async();
+    const packagePath = `${this.ionicAppName}/package.json`;
+    const packageJSON = this.fs.readJSON(packagePath);
+    const CLIENT_MAIN_SRC_DIR = `${this.ionicAppName}/src/`;
+
+    // add some branding 🤓
+    packageJSON.author = 'Ionic Framework + JHipster';
+    packageJSON.homepage = 'https://www.jhipster.tech';
+    packageJSON.description = 'A hipster Ionic project, made with 💙 by @oktadev!';
+    packageJSON.devDependencies['generator-jhipster-ionic'] = packagejs.version;
+    jsonfile.writeFileSync(packagePath, packageJSON);
+
+    if (this.jhipsterAppConfig.authenticationType === 'oauth2') {
+      this.packageName = this.jhipsterAppConfig.packageName;
+      this.packageFolder = this.jhipsterAppConfig.packageFolder;
+
+      let installAuthCmd;
+      const params = '--configUri=http://localhost:8080/api/auth-info --issuer=null --clientId=null';
+      const schematicsVersion = '0.8.3';
+
+      // use `schematics` when testing and expect it to be installed
+      if (this.installDeps) {
+        installAuthCmd = `ng add @oktadev/schematics@${schematicsVersion} ${params}`;
+      } else {
+        packageJSON.devDependencies['@oktadev/schematics'] = schematicsVersion;
+        jsonfile.writeFileSync(packagePath, packageJSON);
+        installAuthCmd = `schematics @oktadev/schematics:add-auth ${params} --skipPackageJson=true`;
+      }
+
+      if (shelljs.exec(`cd ${this.ionicAppName} && ${installAuthCmd}`).code !== 0) {
+        this.error(installAuthCmd);
+        shelljs.exit(1);
+      }
+
+      // force overwriting of files since prompting will confuse developers on initial install
+      this.conflicter.force = true;
+
+      this.log('Updating Ionic AppAuth to work with JHipster...');
+
+      // Update Ionic files to work with JHipster
+      this.template('src/app/app.component.ts', `${CLIENT_MAIN_SRC_DIR}app/app.component.ts`);
+      this.template('src/app/app.component.spec.ts', `${CLIENT_MAIN_SRC_DIR}app/app.component.spec.ts`);
+      this.template('src/app/app.module.ts', `${CLIENT_MAIN_SRC_DIR}app/app.module.ts`);
+      this.template('src/app/app-routing.module.ts', `${CLIENT_MAIN_SRC_DIR}app/app-routing.module.ts`);
+      this.template('src/app/interceptors/auth.interceptor.ts', `${CLIENT_MAIN_SRC_DIR}app/interceptors/auth.interceptor.ts`);
+      this.template('src/app/pages/home/home.page.spec.ts', `${CLIENT_MAIN_SRC_DIR}app/pages/home/home.page.spec.ts`);
+      this.template('src/app/pages/home/home.page.ts', `${CLIENT_MAIN_SRC_DIR}app/pages/home/home.page.ts`);
+      this.template('src/app/pages/login/login.page.html', `${CLIENT_MAIN_SRC_DIR}app/pages/login/login.page.html`);
+      this.template('src/app/pages/login/login.page.spec.ts', `${CLIENT_MAIN_SRC_DIR}app/pages/login/login.page.spec.ts`);
+      this.template('src/app/pages/login/login.page.ts', `${CLIENT_MAIN_SRC_DIR}app/pages/login/login.page.ts`);
+      this.template('src/app/pages/welcome/welcome.page.html', `${CLIENT_MAIN_SRC_DIR}app/pages/welcome/welcome.page.html`);
+      this.template('src/app/pages/welcome/welcome.page.spec.ts', `${CLIENT_MAIN_SRC_DIR}app/pages/welcome/welcome.page.spec.ts`);
+      this.template('src/app/pages/welcome/welcome.page.ts', `${CLIENT_MAIN_SRC_DIR}app/pages/welcome/welcome.page.ts`);
+      this.template('src/app/services/auth/user-route-access.service.spec.ts', `${CLIENT_MAIN_SRC_DIR}app/services/auth/user-route-access.service.spec.ts`);
+      this.template('src/app/services/auth/user-route-access.service.ts', `${CLIENT_MAIN_SRC_DIR}app/services/auth/user-route-access.service.ts`);
+      this.template('src/app/services/login/login.service.spec.ts', `${CLIENT_MAIN_SRC_DIR}app/services/login/login.service.spec.ts`);
+      this.template('src/app/services/login/login.service.ts', `${CLIENT_MAIN_SRC_DIR}app/services/login/login.service.ts`);
+      this.template('src/app/services/user/user.model.ts', `${CLIENT_MAIN_SRC_DIR}app/services/user/user.model.ts`);
+      this.template('src/app/services/user/user.service.spec.ts', `${CLIENT_MAIN_SRC_DIR}app/services/user/user.service.spec.ts`);
+      this.template('src/app/services/user/user.service.ts', `${CLIENT_MAIN_SRC_DIR}app/services/user/user.service.ts`);
+
+      // Delete files no longer used
+      const filesToDelete = [
+        `${CLIENT_MAIN_SRC_DIR}app/login`,
+        `${CLIENT_MAIN_SRC_DIR}app/tab1`,
+        `${CLIENT_MAIN_SRC_DIR}app/pages/signup`,
+        `${CLIENT_MAIN_SRC_DIR}app/services/auth/auth-jwt.service.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/services/auth/auth-jwt.service.spec.ts`,
+      ];
+
+      filesToDelete.forEach((path) => {
+        if (path.endsWith('.ts') || path.endsWith('.html')) {
+          this.deleteFile(path);
+        } else {
+          this.removeDirectory(path);
+        }
+      });
+    } else {
+      this.log('Adding User model and e2e tests...');
+      this.template('src/app/services/user/user.model.ts', `${CLIENT_MAIN_SRC_DIR}app/services/user/user.model.ts`);
+    }
+
+    // Add e2e tests
+    this.authenticationType = this.jhipsterAppConfig.authenticationType;
+    this.template('e2e/pages/login.po.ts', `${this.ionicAppName}/e2e/pages/login.po.ts`);
+    this.template('e2e/spec/login.e2e-spec.ts', `${this.ionicAppName}/e2e/spec/login.e2e-spec.ts`);
+
+    done();
+  }
+
+  deleteFile(path) {
+    // check to see if the file exists before deleting
+    try {
+      fs.unlinkSync(path);
+    } catch (e) {
+      // file already deleted
+    }
+  }
+
+  removeDirectory(path) {
+    if (fs.existsSync(path)) {
+      fs.readdirSync(path).forEach((file, index) => {
+        const curPath = `${path}/${file}`;
+        if (fs.lstatSync(curPath).isDirectory()) { // recurse
+          this.removeDirectory(curPath);
+        } else { // delete file
+          this.deleteFile(curPath);
+        }
+      });
+      fs.rmdirSync(path);
+    }
+  }
+
+  end() {
+    this.log('\nIonic for JHipster App created successfully! 🎉\n');
+    this.log('Run the following commands (in separate terminal windows) to see everything working:\n');
+    this.log(`${chalk.green(`    cd ${this.directoryPath} && ${this.jhipsterAppConfig.buildTool === 'maven' ? './mvnw' : './gradlew'}`)}`);
+    this.log(`${chalk.green(`    cd ${this.ionicAppName} && ionic serve`)}\n`);
+    if (this.interactive) {
+      // force quit; needed because of this.conflicter.force = true
+      process.exit(0);
+    }
+  }
+};
